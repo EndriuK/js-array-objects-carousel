58527f9dfb501a3958e1fa2a004bd93559c6e6af
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-07-13 12:10:13 +0200

    [FIX] fix esercizio base

--- js/script.js
+++ js/script.js
@@ -1,59 +1,80 @@
 const images = [
   {
-    image: "./img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
+    image: "img/01.webp",
+    title: "Marvel's Spiderman Miles Morales",
     text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
   },
   {
-    image: "./img/02.webp",
+    image: "img/02.webp",
     title: "Ratchet & Clank: Rift Apart",
     text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
   },
   {
-    image: "./img/03.webp",
+    image: "img/03.webp",
     title: "Fortnite",
     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
   },
   {
-    image: "./img/04.webp",
+    image: "img/04.webp",
     title: "Stray",
     text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
   },
   {
-    image: "./img/05.webp",
+    image: "img/05.webp",
     title: "Marvel's Avengers",
     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
 
-// 1. recupero elemento dal DOM
-const items = document.querySelector(".items");
-const thumbs = document.querySelector(".thumbs");
+// Definizioni delle variabili
+let itemsContent = ""; // Definizione della variabile che contiene le immagini
+let activeElement = 0; // Definizione della variabile che contiene l'elemento attivo
 
-// creo due variabili per le due immagini attive nel dom
-let itemsContent = "";
-let thumbsContent = "";
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
-// ciclo le immagini per creare i singoli elementi
-images.forEach((image) => {
-  itemsContent += `<div class="item">
-                      <img src="${image.image}" alt="${image.title}">
-                      <div class="text">
-                        <h2>${image.title}</h2>
-                        <p>${image.text}</p>
-                      </div>
-                    </div>`;
-  thumbsContent += `<div class="thumb">
-                      <img src="${image.image}" alt="${image.title}">
-                    </div>`;
+// Ciclo l'array e concateno ad itemsContent l'HTML delle immagini realizzato prima in maniera statica
+images.forEach((elem) => {
+  itemsContent += `
+  <div class="image position-relative h-100 d-none">
+      <img src="${elem.image}" class="main-image" alt="${elem.title}">
+      <div class="txt-image">
+           <h3>${elem.title}</h3>
+           <p>${elem.text}</p>
+      </div>
+  </div>`;
 });
 
-// appendere con innerHTML il contenuto della variabile itemsContent al contenitore items
-items.innerHTML = itemsContent;
+// Vado a recuperare l'elemento del DOM che deve contenere le immagini
+document.querySelector("#my-carousel-container .images").innerHTML =
+  itemsContent;
 
-// appendere con innerHTML il contenuto della variabile thumbsContent al contenitore thumbs
-thumbs.innerHTML = thumbsContent;
+// Recupero il primo elemento con la classe image che ha anche la classe d-none per toglierle quest'ultima
+let allImages = document.querySelectorAll(".image"); // Recupero tutti gli elementi che hanno la classe .image
+allImages[activeElement].classList.remove("d-none"); // Al primo tolgo la classe d-none
 
-// Seleziona il primo elemento come attivo
-document.querySelector(".item").classList.add("active");
-document.querySelector(".thumb").classList.add("active");
+// Definisco il comportamento del click sulla freccia destra
+next.addEventListener("click", function () {
+  allImages[activeElement].classList.add("d-none"); // Aggiungo all'elemento attuale la classe d-none
+
+  if (activeElement === allImages.length - 1) {
+    activeElement = 0;
+  } else {
+    activeElement++; // Incremento l'elemento attivo
+  }
+
+  allImages[activeElement].classList.remove("d-none"); // Tolgo la classe d-none
+});
+
+// Definisco il comportamento del click sulla freccia sinistra
+prev.addEventListener("click", function () {
+  allImages[activeElement].classList.add("d-none"); // Aggiungo all'elemento attuale la classe d-none
+
+  if (activeElement === 0) {
+    activeElement = allImages.length - 1;
+  } else {
+    activeElement--; // Decremento l'elemento attivo
+  }
+
+  allImages[activeElement].classList.remove("d-none"); // Tolgo la classe d-none
+});
